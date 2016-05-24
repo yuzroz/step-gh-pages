bd276ada5186a7d8780068152a6f2af3204f68eb
Yuzuru Ozawa <y.ozawa@trippiece.com>
2016-05-24 14:55:00 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -52,11 +52,11 @@ git config user.email "pleasemailus@wercker.com"
 git config user.name "werckerbot"
 
 git add .
-if [ -n "$WERCKER_GH_PAGES_MESSAGE" ]
+if [ -n $WERCKER_GH_PAGES_MESSAGE ]
 then
   git commit -m "$WERCKER_GH_PAGES_MESSAGE"
 else
-  git commit -m "deploy from $WERCKER_STARTED_BY"
+  git commit -m "deploy from $WERCKER_GIT_OWNER"
 fi
 
 result="$(git push -f $remote master:$branch)"
